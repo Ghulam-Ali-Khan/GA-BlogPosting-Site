18817f38cc634c32e97bdb8f3cc329c6900c431d
umar <umar@netrootstech.com>
2023-07-12 17:23:57 +0500

    Added theme chages icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -101,41 +101,10 @@ const Header = () => {
                     </div>
                     <div class="col-md-9 col-xs-6 text-end header-top-right ">
                         <ul class="list-inline nav-topbar d-none d-md-inline">
-                            <li class="list-inline-item menu-item-has-children"><a href="#">Layouts</a>
-                                <ul class="sub-menu font-small">
-                                    <li class="menu-item-has-children"><a href="#">Pages</a>
-                                        <ul class="sub-menu font-small">
-                                            <li><a href="page-about.html.htm">About</a></li>
-                                            <li><a href="page-contact.html.htm">Contact</a></li>
-                                            <li><a href="page-typography.html.htm">Typography</a></li>
-                                            <li><a href="page-register.html.htm">Register</a></li>
-                                            <li><a href="page-login.html.htm">Login</a></li>
-                                            <li><a href="page-search.html.htm">Search</a></li>
-                                            <li><a href="page-author.html.htm">Author</a></li>
-                                            <li><a href="page-404.html.htm">404 page</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="menu-item-has-children"><a href="#">Category</a>
-                                        <ul class="sub-menu font-small">
-                                            <li><a href="category-list.html.htm">List layout</a></li>
-                                            <li><a href="category-grid.html.htm">Grid layout</a></li>
-                                            <li><a href="category-masonry.html.htm">Masonry layout</a></li>
-                                            <li><a href="category-big.html.htm">Big layout</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="menu-item-has-children"><a href="#">Single post</a>
-                                        <ul class="sub-menu font-small">
-                                            <li><a href="single.html.htm">Default</a></li>
-                                            <li><a href="single-2.html.htm">Big image</a></li>
-                                            <li><a href="single-3.html.htm">Left image</a></li>
-                                            <li><a href="single-4.html.htm">With sidebar</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="list-inline-item"><a href="https://demos.alithemes.com/html/stories/docs/"><i class="elegant-icon icon_document_alt mr-5"></i>Document</a></li>
+                           
+                          
                         </ul>
-                        <span class="vertical-divider mr-20 ml-20 d-none d-md-inline"></span>
+                        
                         <button class="search-icon d-none d-md-inline"><span class="mr-15 text-muted font-small"><i class="elegant-icon icon_search mr-5"></i>Search</span></button>
                         <div class="dark-light-mode-cover">
                             <a class="dark-light-mode" href="#"></a>
